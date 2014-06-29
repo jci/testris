5af75c964e534d44e6282da2acd90e3ceb93a64a
Juan C. Inostroza <jci@codemonkey.cl>
2014-06-29 18:47:56 -0400

    Second commit - here it goes...

---
+++ Source/Game.hx
@@ -0,0 +1,30 @@
+package;
+
+import openfl.Assets;
+import openfl.media.Sound;
+import openfl.media.SoundTransform;
+import SoundManager;
+
+
+
+class Game
+{
+	// main game class
+
+
+	public function new()
+	{
+	}
+
+	public function init()
+	{
+	}
+
+	public function update()
+	{
+	}
+
+	public function destroy()
+	{
+	}
+}
--- Source/Main.hx
+++ Source/Main.hx
@@ -6,25 +6,66 @@ import openfl.display.Sprite;
 import openfl.events.KeyboardEvent;
 import openfl.ui.Keyboard;
 import openfl.Lib;
+import openfl.utils.Timer;
+import openfl.display.Tilesheet;
+import openfl.display.Sprite;
+
 
 import SoundManager;
 
 class Main extends Sprite 
 {
-	
+
+	public var fullscr:Bool;
+
+	var aheight : Int;
+	var awidth : Int;
 	
 	public function new () 
 	{
 		super ();
+		haxe.Timer.delay(init,1000);	
+
+	}
+
+	public function init()
+	{
+		// now, prepare the screen
+
+		var game = new Game();
+
+		aheight = stage.stageHeight;
+		awidth = stage.stageWidth;
+
+
+		addListeners();
 
 		SoundManager.getInstance().loadMusic("assets/tetris.ogg");
 		SoundManager.getInstance().playMusic();
 
+	}
+
+	function createScene()
+	{
+
+	}
+
+	function mainLoop()
+	{
+	}
+
+	function addListeners()
+	{
 		stage.addEventListener(KeyboardEvent.KEY_DOWN,onKeyPressed);
 	}
 
 	function onKeyPressed(event:KeyboardEvent)
 	{
+
+
+
+
+
 		switch (event.keyCode)
 		{
 			case Keyboard.DOWN:
@@ -40,6 +81,9 @@ class Main extends Sprite
 				}
 
 		}
+
+		
+		flash.Lib.trace("is this on" + aheight + " " + awidth);
 	}
 	
 	
